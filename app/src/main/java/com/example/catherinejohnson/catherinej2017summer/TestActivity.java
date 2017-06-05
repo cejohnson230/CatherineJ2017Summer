503ebda64ec640c431481f984cd814ad901c0f50
cejohnson230 <cejohnson230@gmail.com>
2017-06-05 09:57:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/catherinejohnson/catherinej2017summer/TestActivity.java
@@ -0,0 +1,13 @@
+package com.example.catherinejohnson.catherinej2017summer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test); //sets the activty_test layout to this method
+    }
+}
